7cc4ad7de7ce18b67067debac51928c5206d0ede
Khaja Takreem Ulla <90376390+khajatakreemulla@users.noreply.github.com>
2021-11-29 16:31:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 
             
 let Timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-    document.getElementById("timeshown").innerHTML = Timezone
+    
 document.querySelectorAll(".duration").forEach((data , index)=>{
     date = data.getAttribute("data-date");
     let time = new Date(date)
     let x  = document.querySelectorAll(".speakers-time")
-    x[index].innerHTML = time.toLocaleString("en-US", {timeZone: Timezone, timeStyle: "short", hourCycle: "h24"})
+    x[index].innerHTML = time.toLocaleString("en-US", {timeZone: Timezone})
 })
 
+document.getElementById("get").innerHTML = Timezone
   
 
 
