022e8561cf0b90fa83d4edf6d463812ab4265211
Khaja Takreem Ulla <90376390+khajatakreemulla@users.noreply.github.com>
2021-11-29 15:25:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
 
-
-
-// const str = new Date("2021-11-27T10:00:00+05:30").toLocaleString("")
-
+            
 let Timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-
-var d = new Date("2021-11-27T10:00:00+05:30");
-
-document.querySelectorAll(".duration").forEach(data=>{
+    
+document.querySelectorAll(".duration").forEach((data , index)=>{
     date = data.getAttribute("data-date");
-    var x = document.querySelectorAll(".speakers-time");
-    var i = 0;
-    for (i = 0; i < x.length; i++) {
-      x[i].innerHTML = date
-    }
-
+    let time = new Date(date)
+    let x  = document.querySelectorAll(".speakers-time")
+    x[index].innerHTML = time.toLocaleString("en-US", {timeZone: Timezone, timeStyle: "short", hourCycle: "h24"})
 })
-let x = d.toLocaleString("en-US", {timeZone:Timezone, timeStyle:"short", hourCycle:"h24"})
-document.getElementById("ishaq").innerHTML = x
+
+  
+
+
+
 
 
 
