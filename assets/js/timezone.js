e248951242657c423d983219db2306f53e9976b6
Khaja Takreem Ulla <90376390+khajatakreemulla@users.noreply.github.com>
2021-11-29 11:34:21 +0530

    Delete assets directory

--- assets/js/timezone.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $('.speakers-time').each(function () {
-            $time = $(this).data('date')
-            $mydate =  moment.parseZone($time).local().format('HH:mm')
-            $(this).html($mydate);    
-    })
-   
-});
