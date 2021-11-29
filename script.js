e350a0d6eb647c7157976f7c6f0d8cc180db033a
Khaja Takreem Ulla <90376390+khajatakreemulla@users.noreply.github.com>
2021-11-29 15:35:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+
+            
+let Timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    
+document.querySelectorAll(".duration").forEach((data , index)=>{
+    date = data.getAttribute("data-date");
+    let time = new Date(date)
+    let x  = document.querySelectorAll(".speakers-time")
+    x[index].innerHTML = time.toLocaleString("en-US", {timeZone: Timezone, timeStyle: "short", hourCycle: "h24"})
+})
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
